523dc1769073532094b844f1054f0995dbeb6209
Lucas Costa <lucas.costa@simova.com.br>
2025-10-18 09:25:55 -0300

    feat: cria um componente para padronizar o título e subtítulo das views

---
+++ src/components/TitlePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <p v-if="subtitle" class="subtitle-page">{{ subtitle }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProductsTable',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped>
+.subtitle-page {
+  color: rgb(var(--v-theme-cinza_w2));
+}
+</style>
