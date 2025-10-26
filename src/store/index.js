0245909cd15bef4a0aabae6897d6f0258651ce9b
Lucas Costa <lucas.costa@simova.com.br>
2025-10-26 20:28:07 -0300

    feat: implementa o módulo de 'jobs'/serviços dentro da store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
+import jobs from './modules/jobs';
 import products from './modules/products';
 import ui from './modules/ui';
 
 export default createStore({
   modules: {
+    jobs,
     products,
     ui
   }
---
+++ src/store/modules/jobs.js
@@ -0,0 +1,31 @@
+import api from "@/api/axios";
+
+export default {
+  namespaced: true,
+  state: () => ({
+    items: []
+  }),
+  getters: {
+    getItems: (state) => state.items,
+  },
+  mutations: {
+    setItems(state, items) {
+      state.items = items
+    }
+  },
+  actions: {
+    async fetchJobs({ commit, dispatch }) {
+      try {
+        dispatch('ui/startLoading', null, { root: true });
+        const response = await api.get('/job/all');
+        commit('setItems', response.data);
+      } catch (error) {
+        const msgError = 'Erro ao carregar a lista de serviços';
+        dispatch('ui/notify',  { message: msgError, color: 'error' }, { root: true } );
+        console.error('Erro ao carregar serviços: ', error);
+      } finally {
+        dispatch('ui/stopLoading', null, { root: true });
+      }
+    }
+  }
+}
