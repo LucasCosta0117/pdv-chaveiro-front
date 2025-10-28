03fbc93511128539de18b6b6f0755f2c35f8bbc9
Lucas Costa <lucas.costa@simova.com.br>
2025-10-27 23:12:13 -0300

    fix: registra o plugin 'formatCurrency' na main.js como global

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import api from './api/axios'
+import { formatCurrency } from './helpers/formatters'
 
 loadFonts();
 
 const app = createApp(App);
 
 app.config.globalProperties.$api = api;
+app.config.globalProperties.$formatCurrency = formatCurrency;
 
 app.use(router)
   .use(store)
