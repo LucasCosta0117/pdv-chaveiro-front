2be452335caab558220d0e21818219b99cbaa36d
Lucas Costa <lucas.costa@simova.com.br>
2025-11-05 22:26:12 -0300

    feat: cria a base da vuex store de vendas

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore } from 'vuex'
 import jobs from './modules/jobs';
 import products from './modules/products';
+import sales from './modules/sales';
 import ui from './modules/ui';
 
 export default createStore({
   modules: {
     jobs,
     products,
+    sales,
     ui
   }
 })
---
+++ src/store/modules/sales.js
@@ -0,0 +1,59 @@
+import api from "@/api/axios";
+
+export default {
+  namespaced: true,
+  state: () => ({
+    items: []
+  }),
+  getters: {
+    getItems: (state) => state.items,
+  },
+  mutations: {
+    setItems(state, items) {
+      state.items = items
+    }
+  },
+  actions: {
+    async fetchSales({ commit, dispatch }) {
+      try {
+        dispatch('ui/startLoading', null, { root: true });
+        const response = await api.get('/sale/all');
+        commit('setItems', response.data);
+      } catch (error) {
+        const msgError = 'Erro ao carregar o histórico de vendas';
+
+        dispatch('ui/notify',  { 
+          message: msgError, 
+          color: 'error' 
+        }, { root: true } );
+
+        console.error('Erro ao carregar produtos: ', error);
+      } finally {
+        dispatch('ui/stopLoading', null, { root: true });
+      }
+    },
+    async saveSale({ commit, dispatch }) {
+      try {
+        dispatch('ui/startLoading', null, { root: true });
+
+        const response = await api.post('/sale/save', saleData);
+        commit('setItems', response.data);
+
+        dispatch('ui/notify', { 
+          message: 'Venda registrada com sucesso!',
+          color: 'success'
+        }, { root: true });
+      } catch (error) {
+        const msgError = 'Erro ao registrar a venda';
+        dispatch('ui/notify',  { 
+          message: msgError, 
+          color: 'error' 
+        }, { root: true } );
+        
+        console.error('Erro ao salvar venda: ', error);
+      } finally {
+        dispatch('ui/stopLoading', null, { root: true });
+      }
+    }
+  }
+}
