c91daf096a1cf6b615ea6ca3486d4eb4dfd151f4
Lucas Costa <lucas.costa@simova.com.br>
2026-03-23 22:29:18 -0300

    feat: implementa a interceptação durante a navegação entre views e garante a exigência de autenticação para rotas não públicas

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '../store'
 
+/**
+ * Definição das Rotas do sistema.
+ */
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LoginView.vue')
+  },
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: { requiresAuth: true }
   },
   {
     path: '/sales',
     name: 'sales',
-    component: () => import('@/views/SaleStationView.vue')
+    component: () => import('@/views/SaleStationView.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/inventory',
     name: 'inventory',
-    component: () => import('@/views/InventoryView.vue')
+    component: () => import('@/views/InventoryView.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/history',
     name: 'history',
-    component: () => import('@/views/HistoryView.vue')
+    component: () => import('@/views/HistoryView.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/:pathMatch(.*)*',
@@ -35,6 +48,26 @@ const router = createRouter({
   scrollBehavior() {
     // Sempre rola para o topo ao navegar entre páginas
     return { top: 0 }
+  }
+})
+
+/**
+ * Intercepta todas as navegações antes delas acontecerem,
+ * garantindo a proteção de acesso as rotas/views (Navigation Guard).
+ */
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuthenticated = store.getters['auth/isAuthenticated']
+
+  if (requiresAuth && !isAuthenticated) {
+    // Cenário 1: Rota autenticada e sem token -> Redireciona para o Login
+    next({ name: 'login' })
+  } else if (to.name === 'login' && isAuthenticated) {
+    // Cenário 2: Possui token e tentou acessar o Login -> Redireciona para Home
+    next({ name: 'home' })
+  } else {
+    // Cenário 3: Rota pública ou usuário autenticado.
+    next()
   }
 })
 
